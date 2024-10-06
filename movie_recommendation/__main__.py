60c9ce6ca5ea3a9a5b111b076f92e867d8ad8203
KevinGregory <KevinGregory@users.noreply.github.com>
2024-10-06 19:03:30 +0000

    ✅ Ready to clone and code.

---
+++ movie_recommendation/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for movie_recommendation."""
+
+from movie_recommendation.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from movie_recommendation.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "movie_recommendation"
